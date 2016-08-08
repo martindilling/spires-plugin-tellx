d3d84dc1e5b8d0056de61dab6fb680f6c3fe8485
Martin Dilling-Hansen <martin.dilling-hansen@wegottickets.com>
2016-08-08 12:42:38 +0100

    Simplified getting who and what

--- src/TellMessage.php
+++ src/TellMessage.php
@@ -11,61 +11,28 @@ use Spires\Plugins\BangMessage\Inbound\BangMessage;
 class TellMessage extends BangMessage
 {
     /**
-     * @var string
-     */
-    private $who;
-
-    /**
-     * @var string
-     */
-    private $what;
-
-    public function __construct($nickname, $username, $hostname, $serverName, $command, $params)
-    {
-        parent::__construct($nickname, $username, $hostname, $serverName, $command, $params);
-        $this->parse();
-    }
-
-    private function parse()
-    {
-        $who = null;
-        $what = null;
-
-        $text = $this->text();
-
-        $start = strpos($text, ' ') + 1;
-        while (is_null($who) || is_null($what)) {
-            $nextSpace = strpos($text, ' ', $start);
-            $chunk = substr($text, $start, $nextSpace - $start);
-            if (is_null($who)) {
-                $who = $chunk;
-            } else if (is_null($what)) {
-                $what = substr($text, $start);
-            }
-            $start = $nextSpace + 1;
-        }
-
-        $this->who = $who;
-        $this->what = $what;
-    }
-
-    /**
      * Who to send the message to
+     * !tell <who> Hey you
      *
      * @return string
      */
     public function who()
     {
-        return $this->who;
+        list($bang, $who, $what) = explode(' ', $this->text(), 3);
+
+        return $who;
     }
 
     /**
      * The message to send
+     * !tell ascii-soup <what>
      *
      * @return string
      */
     public function what()
     {
-        return $this->what;
+        list($bang, $who, $what) = explode(' ', $this->text(), 3);
+
+        return $what;
     }
 }
